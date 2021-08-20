e35c39c7dcc48673d892edce33ce560056dc8326
Daniel Boadzie <boadziedaniel43@gmail.com>
2021-08-20 10:23:54 +0000

    started carousel website

---
+++ src/lib/Slider.svelte
@@ -0,0 +1,58 @@
+<script>
+    import { onMount } from "svelte";
+  
+    let Carousel;
+    onMount(async () => {
+      const module = await import("svelte-carousel");
+      Carousel = module.default;
+      
+    });
+  
+    const items = [
+      {
+        name: "Leonardo",
+        age: 26,
+        location: "Italy"
+      },
+      {
+        name: "Maria",
+        age: 27,
+        location: "Brazil"
+      },
+      {
+        name: "Oliver",
+        age: 28,
+        location: "United States"
+      },
+      {
+        name: "Margarida",
+        age: 29,
+        location: "Portugal"
+      }
+    ];
+  </script>
+  
+  <svelte:component this={Carousel} let:showPrevPage let:showNextPage>
+          <button
+              type="button"
+              slot="prev"
+              on:click={showPrevPage}
+          >
+              &LongLeftArrow;
+          </button>
+      {#each items as item, i (item.name)}
+      <div style="padding: 30px 8px; text-align: center; background: #EEE; border-radius: 5px;">
+          <strong>{item.name}</strong> ({item.age})
+          from {item.location}
+        </div>
+      {/each}
+          <button
+              type="button"
+              slot="next"
+              on:click={showNextPage}
+          >
+      &LongRightArrow;
+          </button>
+      </svelte:component>
+  
+  
